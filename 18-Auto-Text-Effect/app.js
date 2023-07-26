ea52f416c13bf73fb476f5f5cb1f6b2bca788b14
Osman Enes <80752567+osmanenesunal@users.noreply.github.com>
2023-07-26 16:07:57 +0300

    18-Auto-Text-Effects
    18-Auto Text Effect

---
+++ 18-Auto-Text-Effect/app.js
@@ -0,0 +1,20 @@
+const content = document.getElementById('content')
+const speedEl = document.getElementById('speed')
+const text = 'I am a student studying Management Information Systems, and I aspire to become a Front-end Developer.'
+let idx = 1
+let speed = 300 / speedEl.value
+
+writeText()
+
+function writeText() {
+  content.innerText = text.slice(0, idx)
+  idx++
+
+  if (idx > text.length) {
+    idx = 1
+  }
+
+  setTimeout(writeText, speed)
+}
+
+speedEl.addEventListener('input', (e) => (speed = 300 / e.target.value))
